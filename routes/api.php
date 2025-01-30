0ebef7444ccfea4998b95cd0e12da30ef559dbb7
misi1239 <dankomisi1@gmail.com>
2025-01-30 14:37:44 +0100

    remove unused action end endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,13 @@
 use App\Http\Controllers\Api\Project\CreateAction;
 use App\Http\Controllers\Api\Project\ExportController;
 use App\Http\Controllers\Api\Project\ListAction;
-use App\Http\Controllers\Api\Project\ShowAction;
 use App\Http\Controllers\Api\ProjectDetails\CreateAction as ProjectDetailsCreateAction;
 use App\Http\Controllers\Api\ProjectDetails\GetCompletedDetailsAction;
 use App\Http\Controllers\Api\ProjectDetails\GetLatestProjectDetailAction;
-use App\Http\Controllers\Api\ProjectDetails\ShowAction as ProjectDetailsShowAction;
 use App\Http\Controllers\Api\ProjectDetails\UpdateAction as ProjectDetailsUpdateAction;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/projects', ListAction::class);
-Route::get('/projects/{id}', ShowAction::class);
-Route::get('/projects/{id}/details', ProjectDetailsShowAction::class);
 Route::post('/projects/create', CreateAction::class);
 Route::post('/projects/{id}/details/create', ProjectDetailsCreateAction::class);
 Route::patch('/projects/{projectId}/details/update/{id}', ProjectDetailsUpdateAction::class);
